ddea2b8744b4f54b6a4418ce263223580c12984f
Kimleng <kimleng@github.com>
2018-12-26 21:05:27 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by hor kimleng on 12/26/18.
+//  Copyright © 2018 hor kimleng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
