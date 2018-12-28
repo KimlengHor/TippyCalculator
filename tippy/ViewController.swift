2676ac649727bd6f94fb7f18a30477fabaa2982e
Kimleng <kimleng@github.com>
2018-12-27 17:02:12 -0800

    First version

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var percentageSegmentControl: UISegmentedControl!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    //Variables
+    var result: Double?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        numberTextField.becomeFirstResponder()
+        numberTextField.placeholder = "$"
+        numberTextField.addTarget(self, action: #selector(calculatePecentage), for: .editingChanged)
+        percentageSegmentControl.addTarget(self, action: #selector(calculatePecentage), for: .valueChanged)
     }
-
-
+    
+    @objc fileprivate func calculatePecentage() {
+        
+        guard let input = Double(numberTextField.text!) else {
+            resultLabel.text = ""
+            return
+        }
+        
+        switch percentageSegmentControl.selectedSegmentIndex {
+        case 0:
+            result = (input  * 0.18) + input
+        case 1:
+            result = (input  * 0.20) + input
+        default:
+            result = (input  * 0.25) + input
+        }
+        
+        resultLabel.text = String(format:"%.2f", result ?? "")
+    }
 }
 
